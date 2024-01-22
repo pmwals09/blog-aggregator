186a2d0b60509cada8eb85a89aca28f2a4d7dce0
Patrick Walsh <pmwals09@gmail.com>
2024-01-21 21:16:32 -0500

    Add PSQL and infrastructure

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/pmwals09/rss-aggregator/internal/database"
 )
+
+type apiConfig struct {
+  DB *database.Queries
+}
 
 func main() {
   err := godotenv.Load()
@@ -20,6 +30,18 @@ func main() {
     return
   }
   port := os.Getenv("PORT")
+  dbURL := os.Getenv("DB_URL")
+
+  db, err := sql.Open("postgres", dbURL)
+  if err != nil {
+    fmt.Println("Error connecting to database")
+    os.Exit(2)
+    return
+  }
+
+  dbQueries := database.New(db)
+
+  ac := apiConfig { dbQueries }
 
   r := chi.NewRouter()
   r.Use(cors.Handler(cors.Options{}))
@@ -29,6 +51,33 @@ func main() {
   })
   v1.Get("/err", func(w http.ResponseWriter, r *http.Request) {
     respondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+  })
+  v1.Post("/users", func(w http.ResponseWriter, r *http.Request) {
+    type usersRequest struct {
+      Name string `json:"name"`
+    }
+    decoder := json.NewDecoder(r.Body)
+    defer r.Body.Close()
+    newUsersReq := usersRequest{}
+    err := decoder.Decode(&newUsersReq)
+    if err != nil {
+      respondWithError(w, http.StatusBadRequest, "Could not decode json request")
+      fmt.Println(err)
+      return
+    }
+    user := database.CreateUserParams{
+      ID: uuid.New(),
+      CreatedAt: time.Now(),
+      UpdatedAt: time.Now(),
+      Name: newUsersReq.Name,
+    }
+    newUser, err := ac.DB.CreateUser(context.Background(), user)
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, "Error creating user")
+      fmt.Println(err)
+      return
+    }
+    respondWithJSON(w, http.StatusCreated, newUser)
   })
   r.Mount("/v1", v1)
 
